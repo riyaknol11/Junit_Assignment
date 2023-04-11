5612e303031f596f049bf9adba9f78440d515d80
riyaknol11 <riya.sharma@knoldus.com>
2023-04-12 00:05:14 +0530

    Modified the naming conventions of methods and added required changes following the reviews

--- Junit4/src/test/java/com/knoldus/StringUtilsTest.java
+++ Junit4/src/test/java/com/knoldus/StringUtilsTest.java
@@ -4,7 +4,7 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
-public class StringUtilsTest {
+public class StringUtilsTest{
     StringUtils stringUtils;
 
     @Before
@@ -14,56 +14,50 @@ public class StringUtilsTest {
 
     // reverseStringTestMethod method to test the reverse method on given string
     @Test
-    public void reverseStringTestMethodForNonEmptyString(){
+    public void testReverseString_givenNonNullString_shouldReturnReverseString(){
         String reversedString= stringUtils.reverse("riya");
         assertEquals("ayir",reversedString);
 
     }
 
-    //reverseStringTestMethod method to test the reverse method when string is empty.
     @Test
-    public void reverseStringTestMethodForEmptyString(){
+    public void testReverseString_givenEmptyString_shouldReturnInvalid(){
 
         String reversedString = stringUtils.reverse( "");
         assertEquals("Invalid input", reversedString);
     }
 
-    // stringEmptyOrNot_TestMethod1 method to test when string is empty.
     @Test
-    public void stringEmptyOrNot_TestMethodForEmptyString(){
+    public void testEmptyorNot_givenEmptyString_shouldReturnTrue(){
 
         boolean result = stringUtils.EmptyOrNot("");
         assertEquals(true,result);
     }
 
-    //  stringEmptyOrNot_TestMethod2 method to test if string is empty or not with a non-empty string.
     @Test
-    public void stringEmptyOrNot_TestMethodForNonEmptyString(){
+    public void testEmptyOrNot_givenNonNullString_shouldReturnFalse(){
 
         boolean result = stringUtils.EmptyOrNot("riya");
         assertEquals(false, result);
     }
 
 
-    // testing method to test if string is Palindrome or not
     @Test
-    public void isPalindromeOrNot(){
+    public void testPalindrome_givenPalindromeString_shouldReturnTrue(){
 
         boolean result=stringUtils.isPalindrome("radar");
         assertEquals(true,result);
     }
 
 
-    // testing method to test a non-palindrome string.
-    @Test public void isPalindrome_WithNonPalindromicString(){
+    @Test public void testPalindrome_givenNonPalindrome_shouldReturnFalse(){
         boolean result = stringUtils.isPalindrome("Hello");
         assertEquals(false,result);
     }
 
 
-    // testing method to test if string is Palindrome or not with an empty string.
     @Test
-    public void isPalindrome_WithEmptyString(){
+    public void testPalindrome_givenEmptyString_shouldReturnFalse(){
         assertEquals(false,stringUtils.isPalindrome(""));
     }
     @After
